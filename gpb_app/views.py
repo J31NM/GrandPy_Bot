41da054e942cfc6d8d9e4c69de045d351ee5ce72
J31NM <48386404+J31NM@users.noreply.github.com>
2020-11-09 12:41:26 +0100

    initialize webpage
    _ initialize the web page with the index.html document
    
    _ import 2 modules (Parser and WikiAPI) from api_manager.py in order to parse the user input and get a response from MediaWiki API

--- gpb_app/views.py
+++ gpb_app/views.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
+from gpb_app.api_manager import Parser
+from gpb_app.api_manager import WikiAPI
 
 app = Flask(__name__)
-
-# Config options - Make sure you created a 'config.py' file.
-app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
+app.config.from_object('config')
+parser = Parser()
+wiki = WikiAPI()
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
+
+
+@app.route("/", methods=["GET"])
+def retrieve():
+    return render_template('index.html')
+
+
+@app.route("/sendRequest/")
+def results():
+    parsed_result = parser.parse(request.args.get("query"))
+    output = wiki.get_wiki_output(parsed_result)
+    return jsonify(output)
 
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
