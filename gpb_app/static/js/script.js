7f397811c6bac88db6eaa267f55111ca716561b5
J31NM <48386404+J31NM@users.noreply.github.com>
2021-01-22 11:32:21 +0100

    Update script.js
    Add GrandPy-Bot interraction in function onResponse()

--- gpb_app/static/js/script.js
+++ gpb_app/static/js/script.js
@@ -1,9 +1,17 @@
+let loadingGif = document.getElementById("loading");
+
+$( window ).on( "load", function() {
+    var text = greetings()
+    setInterval(createTextElement(text), 5000);
+});
 
 $(document).ready(function() {
+    // document on load
     $("#search").click(function(event) {
         event.preventDefault()
         var query = $("#query").val()
-        createTextElement(query)
+        createTextElement(query, 1)
+        loadingGif.hidden = false;
         // alert($("#query").val())
         var searchReq = $.getJSON("/sendRequest/", {"query": query});
         searchReq.done(onResponse);
@@ -13,26 +21,57 @@ $(document).ready(function() {
 
 var mapcounter = 0
 
-/*  */
+function greetings() {
+        var examples = [
+            "Salut fiston ! Ca faisait longtemps...... De quoi veux tu parler ?",
+             "Tiens mais qui voici ? tu es venu rançonner papy ? ",
+             "Qui êtes vous ? que me voulez vous ?",
+             "Papy a eu plusieurs vies, il sait tout ! essai de me coller !"
+        ];
+        var example = examples[Math.floor(Math.random() * examples.length)];
+        return example
+    }
+
 function createMapElement() {
     var mapID = 'map' + mapcounter;
-    $("#chat").append('<div id="' + mapID +'" class="map_test" ></div>');
-    mapcounter ++;
-    return mapID
+    if (mapcounter === 0) {
+        mapcounter ++;
+        return
+    } else {
+        $("#chat").append('<div id="' + mapID +'" class="map_test" ></div>');
+        mapcounter ++;
+        return mapID
+    }
+
 }
 
-function createTextElement(text) {
-    $("#chat").append('<div id="papy_text" class="papy_text">' + text + '</div>');
+function createTextElement(text, i=0) {
+    if (i===1) {
+        $("#chat").append('<div id="user_text" class="user_text">' + text + '</div>');
+    } else {
+        $("#chat").append('<div id="papy_text" class="papy_text">' + text + '</div>');
+    }
+
 }
 
 function onResponse(data) {
+    console.log(data);
     var coordinates = data.coordinates
-    // $("#url").attr("href", data.result);
-    initMap(coordinates.lat, coordinates.lng)
-    createTextElement(data.response)
+    var text = data.text
+    var answer = data.answer
+    var error = data.error_message
+    if (error) {
+        createTextElement(error);
+        loadingGif.hidden = false;
+    } else {
+        setTimeout(createTextElement(answer), 3000);
+        createTextElement(text.grandPy_knowledge);
+        setTimeout(createTextElement("Et ça se situe ici :"), 3000);
+        initMap(coordinates.coordinates.lat, coordinates.coordinates.lng);
+        loadingGif.hidden = true;
+    }
 }
 
-/* display a googlemap with 2 variables lat and lon */
 function initMap(lat, lng) {
     if (lat === undefined){
         lat = 48.858093
@@ -43,7 +82,9 @@ function initMap(lat, lng) {
   var location = {lat: parseFloat(lat), lng: parseFloat(lng)};
   var mapID = createMapElement();
   var map = new google.maps.Map(
-      document.getElementById(mapID), {zoom: 11, center: location});
+      document.getElementById(mapID), {zoom: 12, center: location});
   var marker = new google.maps.Marker({position: location, map: map});
+  console.log(marker);
 }
+
 
