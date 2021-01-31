40412c2eb8b5389277e95cd7a54c5c6bc55c8423
J31NM <48386404+J31NM@users.noreply.github.com>
2021-01-31 21:36:26 +0100

    Update test_api_manager.py
    Add test_get_wiki_text

--- tests/test_api_manager.py
+++ tests/test_api_manager.py
@@ -1,8 +1,16 @@
+#coding : UTF-8
+# pylint: disable=C0111
+
+"""
+Test file for main script api_manager.py
+"""
+
 import unittest
 from gpb_app.api_manager import Parser, WikiAPI, MapsAPI
 
 
 class TestParser(unittest.TestCase):
+    """ Test Parser class"""
 
     def setUp(self):
         self.parser = Parser()
@@ -22,6 +30,7 @@ class TestParser(unittest.TestCase):
 
 
 class TestWikiApi(unittest.TestCase):
+    """ Test WikiAPI class"""
 
     def setUp(self):
         self.wikiApi = WikiAPI()
@@ -36,10 +45,11 @@ class TestWikiApi(unittest.TestCase):
         self.assertEqual(self.wikiApi.get_wiki_pageid(words2), {})
         self.assertEqual(self.wikiApi.get_wiki_pageid(words3), {})
 
+    def test_get_wiki_text_return_data(self):
+        self.assertTrue(type(self.wikiApi.get_wiki_text("londres", 4924)) is str)
 
-    # def test_get_wiki_text(self):
-    #     results = "Londres [lɔ̃dʁ]  (en anglais : London [ˈlʌndən] ) est la capitale et la plus grande ville d'Angleterre et du Royaume-Uni,. La ville est située près de l'estuaire de la Tamise dans le sud-est de l'Angleterre. Londinium a été fondée par les Romains il y a presque 2 000 ans. La Cité de Londres, le noyau historique de Londres avec une superficie de seulement 1,12 miles carrés (2,9 km²) conserve des frontières qui suivent de près ses limites médiévales. Londres est gouvernée par le maire de Londres et l'Assemblée de Londres. Londres est considérée comme l'une des villes mondiales les plus importantes du monde,,. La ville exerce un impact considérable sur les arts, le commerce, l'éducation, le divertissement, la mode, les finances, les soins de santé, les médias, les services professionnels, la recherche et le développement, le tourisme et les transports,. Londres se classe 26e sur 300 grandes villes pour ses performances économiques. C'est l'un des plus grands centres financiers avec New York et Hong Kong, et a le cinquième ou le sixième plus gros PIB urbain mondial. C'est la ville la plus visitée mesurée par les arrivées internationales et possède le système aéroportuaire le plus fréquenté par le trafic de passagers du monde."
-    #     self.assertEqual(self.wikiApi.get_wiki_text("londres", 4924), results)
+    def test_get_wiki_text_return_nothing(self):
+        self.assertTrue(type(self.wikiApi.get_wiki_text("egsrger", None)) is dict)
 
     def test_get_grandpy_text(self):
         sentences = "j'aime les pates. j'irai bien traverser les plaines de Mongolie. Saucisse." \
@@ -49,6 +59,7 @@ class TestWikiApi(unittest.TestCase):
 
 
 class TestMapsAPI(unittest.TestCase):
+    """ Test MapsAPI class"""
 
     def setUp(self):
         self.maps = MapsAPI()
@@ -57,10 +68,8 @@ class TestMapsAPI(unittest.TestCase):
         words = ["paris"]
         self.assertEqual(self.maps.get_maps_output(words)["coordinates"], {'lat': 48.856614,
                                                                            'lng': 2.3522219})
+        self.assertEqual(self.maps.get_maps_output(words)["address"], 'Paris, France')
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
